b0a2a0205fd31a25fabbe7f9e70638d1b5b602a8
aaronmatt1 <mmchatton1@gmail.com>
2014-04-06 18:11:59 -0500

    Connect to DB
    checking for DB connection and uploading patients

--- GTMS.py
+++ GTMS.py
@@ -1,23 +1,48 @@
 ##GTMS UI
 
+from tkinter import *
 import base64
+import pymysql
 
 class GTMS:
     def __init__(self, win):
 
-        LoginPage()
+        self.connect()
 
-    def loginPage(self):
+    # def loginPage(self):
+    #
+    #     topFrame = Frame(LogWin)
+    #     topFrame.grid(row=0, colum=0)
+    #     topFrame.configure(background='#c1a82f')
+    #     bottomFrame = Fram(LogWin)
+    #     bottomFrame.grid(row=1, column=0)
+    #
+    #     Label(text="Georgia Tech Medical Center System", font=('Arial', 20)).pack
 
-        topFrame = Frame(LogWin)
-        topFrame.grid(row=0, colum=0)
-        topFrame.configure(background='#c1a82f')
-        bottomFrame = Fram(LogWin)
-        bottomFrame.grid(row=1, column=0)
+    #def newPatientPage(self):
 
-        Label(text="Georgia Tech Medical Center System", font=('Arial', 20)).pack
+    def connect(self):
+
+        try:
+            self.db = pymysql.connect(host="academic-mysql.cc.gatech.edu", #connecting to the database
+                                passwd='KDKR2YQY', user='cs4400_Group_65',
+                                db='cs4400_Group_65')
+            self.c = self.db.cursor()
 
 
+
+        except:
+
+            mbox.showerror(title='Connection Error', message='Check your internet connection')
+            return NONE
+
+        query = 'INSERT INTO USER(Username, Password) VALUES("FlorianF", "12345")'
+        self.c.execute(query)
+
+        query = 'INSERT INTO PATIENT(Name,HomePhone,Username,DOB,Gender,Address,WorkPhone,Height,Weight)' \
+                'VALUES("Florian Foerester","706-595-1000", "FF123", "1986-04-01", "M", "1423 Burgamy Ln.", "706-222-9373", "62", "180")'
+        self.c.execute(query)
+        self.db.close()
 
 LogWin = Tk()
 LogWin.title('GTMS Login')
